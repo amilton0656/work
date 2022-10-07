e3da5309064aa4ed6cd23c3493f40614655858ed
amilton0656 <amilton0656@gmail.com>
2022-10-07 17:29:22 -0300

    2022 10 07

--- back/controllers/co_sienge.js
+++ back/controllers/co_sienge.js
@@ -8,15 +8,29 @@ exports.getAllClientes = (req, res, next) => {
 
   const encodedToken = base64.encode('cotaemp-api:O5n3fO1Qi9HTm5JJgShjauNe53isi0s9')
 
-  axios.get('https://api.sienge.com.br/cotaemp/public/api/v1/customers', 
+  const params = req.query
+  // console.log(req)
+
+  // axios.get('https://api.sienge.com.br/cotaemp/public/api/v1/customers', 
+  axios.get('https://api.sienge.com.br/cotaemp/public/api/v1/units',
             {
-                headers: {
+              params : params,  
+              // params : {
+              //   'limit': 3,
+              //   'enterpriseId': 8,
+              //   'offset': 0
+              // },
+              
+              headers: {
                   'Authorization': `Basic ${encodedToken}` 
-                          }
-                        })
+                          },
+                        }
+   
+                        
+                        )
                 .then(resposta => {
-                    console.log('sim', resposta.data)
-                    res.status(200).json(resposta.data.results)
+                    // console.log('sim', resposta.data)
+                    res.status(200).json(resposta.data)
                     
     
                 })
--- front/src/modulos/sienge/TesteSiengeLista.js
+++ front/src/modulos/sienge/TesteSiengeLista.js
@@ -10,13 +10,21 @@ import {decode as base64_decode, encode as base64_encode} from 'base-64';
 const TesteSiengeLista = () => {
 
     const [dataAll, setDataAll] = useState([])
+    const [sieCount, sieCountSet] = useState(1000)
+    const [sieOffset, OffsetSet] = useState(0)
+    const [sieLimit, sieLimitSet] = useState(0)
+
 
     const token = ''
 
     const getData = (api) => {
-        clienteAxios.get(api, { headers: { Authorization: token } })
+        clienteAxios.get(api, { headers: { Authorization: token }, 
+            params : { 'limit': 2, 'enterpriseId': 8, 'offset': 0 } })
             .then(resposta => {
-                setDataAll(resposta.data)
+                setDataAll(resposta.data.results)
+                sieCountSet(resposta.data.resultSetMetadata.count)
+                OffsetSet(resposta.data.resultSetMetadata.offset)
+                sieLimitSet(resposta.data.resultSetMetadata.limit)
             })
             .catch(err => {
             })
@@ -28,13 +36,17 @@ const TesteSiengeLista = () => {
         getData('/sienge')
     }, [])
 
-    console.log(dataAll)
+
 
     return (
         <>
+        {
+            // metadata.offset
+        }
             {
                 dataAll.map((reg, i) => (
-                    <div key={i}> ----      {reg.name} - {reg.cpf}</div>
+                    <div key={i}> ----      {reg.name} - {reg.commercialStock}</div>
+                    // <div key={i}> ----      {reg.name} - {reg.cpf}</div>
                 ))
 
             }
